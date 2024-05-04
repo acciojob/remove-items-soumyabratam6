453adc94de97da6fc3e8b6392f041149ffad5f1f
soumyabratam6 <144446453+soumyabratam6@users.noreply.github.com>
2024-05-04 19:37:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+            const colorSelect = document.getElementById('colorSelect');
+            const removeButton = document.querySelector('input[type="button"]');
+
+            removeButton.addEventListener('click', function() {
+                const selectedOption = colorSelect.options[colorSelect.selectedIndex];
+                colorSelect.removeChild(selectedOption);
+            });
+        });
